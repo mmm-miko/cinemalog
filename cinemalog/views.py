b1a4dc633d01cd8e2135278547159e735af8a90b
mikohomma <16.honma.m@gmail.com>
2024-10-29 00:57:00 +0900

    画像表示

--- cinemalog/views.py
+++ cinemalog/views.py
@@ -1,3 +1,8 @@
-from django.shortcuts import render
+# from django.shortcuts import render
+from django.views.generic import ListView
+from .models import WatchList
 
 # Create your views here.
+
+class WathListView(ListView):
+    model = WatchList
--- config/urls.py
+++ config/urls.py
@@ -17,7 +17,13 @@ Including another URLconf
 
 from django.contrib import admin
 from django.urls import path
+from django.conf import settings
+from django.conf.urls.static import static
+from cinemalog.views import WathListView
 
 urlpatterns = [
     path("admin/", admin.site.urls),
+    path("", WathListView.as_view(), name="index"),
 ]
+
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
